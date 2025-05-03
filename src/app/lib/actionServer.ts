1e3adfd490dd9334f4a8dc03fc9f99ef38a546e7
❄ Fleur de Blue <135421389+Xylphy@users.noreply.github.com>
2025-05-03 13:58:03 +0800

    Testing action server

--- src/app/lib/actionServer.ts
+++ src/app/lib/actionServer.ts
@@ -1,23 +1,34 @@
 "use server";
 
-import { signupWithFirebase } from "./firebase/action";
+import { sendEmailVerification } from "./firebase/action";
 
 export async function signup(formData: FormData) {
-  console.log("File data", formData.get("resume"));
+  try {
+    // sendEmailVerification({
+    //   prefix: formData.get("prefix") as string,
+    //   firstName: formData.get("firstName") as string,
+    //   lastName: formData.get("lastName") as string,
+    //   email: formData.get("email") as string,
+    //   countryCode: formData.get("countryCode") as string,
+    //   mobileNumber: formData.get("mobileNumber") as string,
+    //   street: formData.get("street") as string,
+    //   zip: formData.get("zip") as string,
+    //   city: formData.get("city") as string,
+    //   state_: formData.get("state") as string,
+    //   country: formData.get("country") as string,
+    //   jobTitle: formData.get("jobTitle") as string,
+    //   skillSet: formData.get("skillSet") as string,
+    // });
 
-  signupWithFirebase({
-    prefix: formData.get("prefix") as string,
-    firstName: formData.get("firstName") as string,
-    lastName: formData.get("lastName") as string,
-    email: formData.get("email") as string,
-    countryCode: formData.get("countryCode") as string,
-    mobileNumber: formData.get("mobileNumber") as string,
-    street: formData.get("street") as string,
-    zip: formData.get("zip") as string,
-    city: formData.get("city") as string,
-    state_: formData.get("state") as string,
-    country: formData.get("country") as string,
-    jobTitle: formData.get("jobTitle") as string,
-    skillSet: formData.get("skillSet") as string,
-  });
+    return {
+      success: true,
+      message: "Email sent successfully",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message:
+        error instanceof Error ? error.message : "Failed to create account",
+    };
+  }
 }
--- src/app/lib/firebase/action.ts
+++ src/app/lib/firebase/action.ts
@@ -5,23 +5,19 @@ import { redirect } from "next/navigation";
 import { RegisterState } from "@/app/types/types";
 import { insertTokenData } from "../mongodb/action";
 
-export async function signupWithFirebase(data: RegisterState) {
-  try {
-    const querySnapshot = await getDocs(
-      query(collection(db, "users"), where("email", "==", data.email))
-    );
+export async function sendEmailVerification(data: RegisterState) {
+  const querySnapshot = await getDocs(
+    query(collection(db, "users"), where("email", "==", data.email))
+  );
 
-    if (!querySnapshot.empty) {
-      redirect("/login");
-    }
-
-    const response = await insertTokenData(data);
-
-    await sendSignInLinkToEmail(auth, data.email, {
-      url: `http://localhost:3000/verification/${response?.insertedId}`,
-      handleCodeInApp: true,
-    });
-  } catch (error) {
-    console.error(error);
+  if (!querySnapshot.empty) {
+    redirect("/login");
   }
+
+  const response = await insertTokenData(data);
+
+  await sendSignInLinkToEmail(auth, data.email, {
+    url: `http://localhost:3000/verification/${response?.insertedId}`,
+    handleCodeInApp: true,
+  });
 }
